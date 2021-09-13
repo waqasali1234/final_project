ebe3354b6acd3d22b284bc4a8824c6b4659cc009
waqasali1234 <waqas6782190@gmail.com>
2021-09-13 12:14:46 +0500

    view

---
+++ resources/views/underconstruction.blade.php
@@ -0,0 +1,8 @@
+@extends("templates.public")
+@section("content")
+    <div class="container" style="margin-top:100px">
+        <h3 align="center">Area under construction</h3>
+        <p align="center">Please come back later</p>
+    </div>
+
+@endsection
